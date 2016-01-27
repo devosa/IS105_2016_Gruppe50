d160c4d0d0236686a67c2fadce46d653a5f54a10
Erlend <erlend_kasin2@hotmail.no>
2016-01-27 14:31:50 +0100

    adding sub function

--- test_add_is105.py
+++ test_add_is105.py
@@ -1,12 +1,17 @@
 import unittest
-from IS105 import add
+from IS105 import add, sub
 
 class TestAdd(unittest.TestCase):
     def setUp(self):
         pass
     def test_numbers_5_7(self):
         self.assertEqual(add(5,7), 11)
-        
+
+class TestSub(unittest.TestCase):
+    def setUp(self):
+        pass
+    def test_numbers_10_2(self):
+        self.assertEqual(sub,10,2), 8)
+
 if __name__ == '__main__':
     unittest.main()
-    
