12ee448833c1f02d924d3480dcd900a5c9af1255
Vegard Aven <vegardaven@Vegards-MacBook-Pro.local>
2016-01-27 15:27:48 +0100

    Add divide

--- IS105.py
+++ IS105.py
@@ -2,3 +2,6 @@
 
 def add(x,y):
     return x + y
+
+def divide(x,y):
+    return x / y
--- test_add_is105.py
+++ test_add_is105.py
@@ -1,11 +1,17 @@
 import unittest
-from IS105 import add
+from IS105 import add, divide
 
 class TestAdd(unittest.TestCase):
     def setUp(self):
         pass
     def test_numbers_5_7(self):
         self.assertEqual(add(5,7), 11)
+        
+class TestDivide(unittest.TestCase):
+    def setUp(self):
+        pass
+    def test_numbers_20_10(self):
+        self.assertEqual(divide(20,10), 2)
         
 if __name__ == '__main__':
     unittest.main()
