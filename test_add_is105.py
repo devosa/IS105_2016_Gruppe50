38308335d5d6472e882e9ed9e22b123016deb915
Dan Marius Holter <Dan Marius Holter>
2016-01-27 16:03:34 +0100

    Add remainder

--- test_add_is105.py
+++ test_add_is105.py
@@ -1,11 +1,17 @@
 import unittest
-from IS105 import add
+from IS105 import add, remain
 
 class TestAdd(unittest.TestCase):
     def setUp(self):
         pass
     def test_numbers_5_7(self):
         self.assertEqual(add(5,7), 11)
+        
+class TestRemain(unittest.TestCase):
+    def setUp(self):
+        pass
+    def test_numbers_4_3(self):
+        self.assertEqual(remain(4,3), 2)            
         
 if __name__ == '__main__':
     unittest.main()
