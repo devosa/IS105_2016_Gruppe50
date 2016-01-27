85b6f0a7105d1ccf8b4c8e24f8df7ae2771af908
tjodalv2k <svennsorensen@gmail.com>
2016-01-27 21:34:50 +0100

    made test pass

--- test_add_is105.py
+++ test_add_is105.py
@@ -5,8 +5,7 @@ class TestAdd(unittest.TestCase):
     def setUp(self):
         pass
     def test_numbers_5_7(self):
-        self.assertEqual(add(5,7), 11)
-        
+        self.assertEqual(add(5,7), 12)
+
 if __name__ == '__main__':
     unittest.main()
-    
