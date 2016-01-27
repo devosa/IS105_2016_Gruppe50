157ea8dd9c6250cb5ea579e796123ec25966adaf
andersskroder <andersskroder@gmail.com>
2016-01-27 17:04:19 +0100

    Adding square function

--- IS105.py
+++ IS105.py
@@ -2,3 +2,6 @@
 
 def add(x,y):
     return x + y
+
+def square(x,y):
+    return x ** y
--- test_add_is105.py
+++ test_add_is105.py
@@ -1,11 +1,17 @@
 import unittest
-from IS105 import add
+from IS105 import add, square
 
 class TestAdd(unittest.TestCase):
     def setUp(self):
         pass
     def test_numbers_5_7(self):
         self.assertEqual(add(5,7), 11)
+
+class TestSquare(unittest.TestCase):
+    def setUp(self):
+        pass
+    def test_numbers_2_2(self):
+        self.assertEqual(square(2,2), 3)
         
 if __name__ == '__main__':
     unittest.main()
