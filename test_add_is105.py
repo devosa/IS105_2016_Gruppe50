041005e6699e3c10e2bdc77e34bc3b1689949899
Erlend <erlend_kasin2@hotmail.no>
2016-01-27 14:48:47 +0100

    fixing function

--- test_add_is105.py
+++ test_add_is105.py
@@ -5,13 +5,13 @@ class TestAdd(unittest.TestCase):
     def setUp(self):
         pass
     def test_numbers_5_7(self):
-        self.assertEqual(add(5,7), 11)
+        self.assertEqual(add(5,7), 12)
 
 class TestSub(unittest.TestCase):
     def setUp(self):
         pass
     def test_numbers_10_2(self):
-        self.assertEqual(sub,10,2), 8)
+        self.assertEqual(sub(10,2), 8)
 
 if __name__ == '__main__':
     unittest.main()
